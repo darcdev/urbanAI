eb81d59bd970d455a4396e54837fc79d531ad1f6
Yuberley <yuberleyguerrero1999@gmail.com>
2025-12-06 23:17:44 -0500

    Add id locations and allow cors

--- backend/Urban.AI/src/Urban.AI.Application/Geography/Dtos/DepartmentResponse.cs
+++ backend/Urban.AI/src/Urban.AI.Application/Geography/Dtos/DepartmentResponse.cs
@@ -1,6 +1,7 @@
 namespace Urban.AI.Application.Geography.Dtos;
 
 public record DepartmentResponse(
+    Guid Id,
     string DepartmentDaneCode,
     string Name,
     decimal? Latitude = null,
--- backend/Urban.AI/src/Urban.AI.Application/Geography/Dtos/TownshipResponse.cs
+++ backend/Urban.AI/src/Urban.AI.Application/Geography/Dtos/TownshipResponse.cs
@@ -1,6 +1,7 @@
 namespace Urban.AI.Application.Geography.Dtos;
 
 public record TownshipResponse(
+    Guid Id,
     string TownshipDaneCode,
     string Name,
     string MunicipalityDaneCode);
